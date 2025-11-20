6ac996c657b30006372479638a5d46ae98a2a601
denilsonali2020 <denilsontrochez32@gmail.com>
2025-11-20 08:05:47 -0600

    Email configurado con mailtrap

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,36 +1,36 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 // Looking to send emails in production? Check out our Email API/SMTP product!
 const emailRegistro = async (datos) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
 
-  //enviar el email
-  const { email, nombre, token } = datos;
+    //enviar el email
+    const { email, nombre, token } = datos;
 
-  //dentro de sendMail va el cuerpo del email
-  await transporter.sendMail({
-    from: '"APV - Administrador de Pacientes de Veterinaria" <no-reply@test-vz9dlem51k14kj50.mlsender.net>',
-    to: email,
-    subject: "Comprueba tu cuenta en APV",
-    text: "Compureba tu cuenta en APV",
-    html: `<p>Hola ${nombre}, comprueba tu cuenta en APV.</p>
+    //dentro de sendMail va el cuerpo del email
+    await transporter.sendMail({
+        from: 'APV - Administrador de Pacientes de Veterinaria',
+        to: email,
+        subject: 'Comprueba tu cuenta en APV',
+        text: 'Compureba tu cuenta en APV',
+        html: `<p>Hola ${nombre}, comprueba tu cuenta en APV.</p>
             <p>Tu cuenta ya esta lista, solo debes comprobarla en el siguiente enlace:
                 <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprueba tu Cuenta Aqui!</a>
             </p>
             
             <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>        
         `,
-  });
-};
+    });
+}
 
-export { emailRegistro };
+
+export {
+    emailRegistro,
+}
